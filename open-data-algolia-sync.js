cf02d1aa611a997fe8a734979c178975869b9931
Jason Lally <jason.lally@gmail.com>
2017-08-19 13:20:17 -0700

    Add moment

--- open-data-algolia-sync.js
+++ open-data-algolia-sync.js
@@ -1,5 +1,6 @@
 var algoliasearch = require('algoliasearch')
 var request = require('request-promise')
+var moment = require('moment')
 
 module.exports = function(ctx, cb) {
   var client = algoliasearch(ctx.data.ALGOLIA_ID, ctx.data.ALGOLIA_SECRET)
